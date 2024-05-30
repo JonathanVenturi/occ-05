eed9d6b29dfe953f181cecce2300e66d3d6197ba
Jonathan Venturi <jonathan.venturi@gmail.com>
2024-05-30 17:45:00 +0200

    Solved exercices

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,14 @@
 //votre code ici
 
+function printNumbers(n) {
+    let output = '';
+    for (let i = 1; i <= n; i++) {
+        output = output + i;
+        if (i < n) {
+            output = output + ' ';
+        }
+    }
+    return output;
+}
+
 export default printNumbers
--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'joqd1c',
   e2e: {
     baseUrl: 'http://127.0.0.1:8080',
     setupNodeEvents(on, config) {
